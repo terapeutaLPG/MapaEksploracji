80a48fdc3c2b6af99c84a697f6cf85cd9253f11b
terapeutaLPG <terapeutaLPG@gmail.com>
2025-05-20 21:34:42 +0200

    mainactivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("com.google.gms.google-services")
 }
 
+
 // dalej kod android { ... } i dependencies { ... }
 
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,27 @@
+//pluginManagement {
+//    repositories {
+//        google()
+//        gradlePluginPortal()
+//        mavenCentral()
+//    }
+//
+//    plugins {
+//        id("com.android.application") version "8.2.2" apply false
+//        id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+//        id("com.google.gms.google-services") version "4.4.0" apply false
+//    }
+//}
+//
+//dependencyResolutionManagement {
+//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+//    repositories {
+//        google()
+//        mavenCentral()
+//    }
+//}
+//
+//rootProject.name = "MapaEksploracji"
+//include(":app")
 pluginManagement {
     repositories {
         google()
